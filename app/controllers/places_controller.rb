e47ab2c711b0257660d67b80e0dee9526b4c2a2e
Richard Sachek <rsachek@gmail.com>
2018-07-03 04:27:56 +0000

    heroku not working

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -9,9 +9,9 @@ class PlacesController < ApplicationController
     @place = Place.new
   end
 
-  def create
-    current_user.places.create(place_params)
-    redirect_to root_path
+ def create
+  current_user.places.create(place_params)
+  redirect_to root_path
   end
 
 end
