cf63ed6f5a8308f13d807f63e0e4130d73842fac
Richard Sachek <rsachek@gmail.com>
2018-07-05 17:47:23 +0000

    restrict destroy

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -31,13 +31,16 @@ class PlacesController < ApplicationController
     if @place.user != current_user
       return render plain: 'Not Allowed', status: :forbidden
     end
-    
+
     @place.update_attributes(place_params)
     redirect_to root_path
   end
 
   def destroy
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
     @place.destroy
     redirect_to root_path
   end
