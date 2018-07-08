fefc5414c4be0b93accb06e3ac95c8d7f2712627
Richard Sachek <rsachek@gmail.com>
2018-07-08 18:37:19 +0000

    Almost done with photo feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180708155157) do
+ActiveRecord::Schema.define(version: 20180708174729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20180708155157) do
     t.integer  "place_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "picture"
     t.index ["place_id"], name: "index_photos_on_place_id", using: :btree
   end
 
