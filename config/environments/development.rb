4751855e44a5f9fe3c8d9799aceec2b967ea4816
Richard Sachek <rsachek@gmail.com>
2018-07-12 15:02:47 +0000

    email back to test mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+  config.action_mailer.delivery_method = :smtp
+
+
   config.action_mailer.default_url_options = { host: 'localhost', port: 3030 }
 
   config.cache_classes = false
@@ -24,7 +27,7 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
