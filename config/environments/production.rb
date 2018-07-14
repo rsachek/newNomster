81e4c6aae8c7fefed710e43ac4eb46d0f6e61f2e
Richard Sachek <rsachek@gmail.com>
2018-07-14 20:06:11 +0000

    Fixed production place link

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
   
-  config.action_mailer.default_url_options = { host: 'newnomster-richard-sachek.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'thirdnomster.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
